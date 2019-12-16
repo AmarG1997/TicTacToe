ea00d42456c1ca2c0a2c7c30f17073a7c6a71813
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-16 11:21:29 +0530

    Added : Assigned letter 'x' or 'o' to player/computer

--- tictactoe.sh
+++ tictactoe.sh
@@ -7,6 +7,8 @@ NO_OF_ROW_COLUMN=3
 BOARD_SIZE=$(($NO_OF_ROW_COLUMN*$NO_OF_ROW_COLUMN))
 
 declare -a TicTacToeBoard
+player=""
+computer=""
 
 function resetBoard()
 {
@@ -15,6 +17,26 @@ function resetBoard()
 		TicTacToeBoard[$places]="-"
 	done
 }
-resetBoard
 
+function letterAssigned()
+{
+	letter=$((RANDOM%2))
+	if [ $letter -eq 0 ]
+	then
+		player="x"
+		computer="0"
+	else
+		computer="x"
+		player="0"
+	fi
+}
+
+function main()
+{
+resetBoard
 echo ${TicTacToeBoard[@]}
+letterAssigned
+echo "player : "$player
+echo "Computer : "$computer
+}
+main
