e87184777903373ae7e86250efd8b38b327d831c
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-16 11:08:35 +0530

    Added : resetBoard function

--- tictactoe.sh
+++ tictactoe.sh
@@ -2,3 +2,19 @@
 
 echo "Welcome To The TicTacToe"
 
+NO_OF_ROW_COLUMN=3
+
+BOARD_SIZE=$(($NO_OF_ROW_COLUMN*$NO_OF_ROW_COLUMN))
+
+declare -a TicTacToeBoard
+
+function resetBoard()
+{
+	for (( places=0; places<$BOARD_SIZE; places++ ))
+	do
+		TicTacToeBoard[$places]="-"
+	done
+}
+resetBoard
+
+echo ${TicTacToeBoard[@]}
