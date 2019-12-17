c6eff33242c26df645bf6f9791906223b3d8e833
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-17 15:48:22 +0530

    Added : function to check row,column,diag win

--- tictactoe.sh
+++ tictactoe.sh
@@ -61,20 +61,42 @@ function selectSell()
 	while [ $counter -le 9 ]
 	do
 		read -p "Enter Cell Number" cell
-		if [ ${TicTacToeBoard[$cell]} == "x" ] || [ ${TicTacToeBoard[$cell]} == "0" ]
-		then
-			echo "Select another cell"
-			counter=$(($counter-1))
-		else
-			flag=1
-		fi
+			if [ ${TicTacToeBoard[$cell]} == "x" ] || [ ${TicTacToeBoard[$cell]} == "0" ]
+			then
+				echo "Select another cell"
+				counter=$(($counter-1))
+			else
+				flag=1
+			fi
 
-		if [ $flag -eq 1 ]
-		then
-			TicTacToeBoard[$cell]=$player
-			counter=$(($counter+1))
-			echo "board display"
-			displayBoard
+			if [ $flag -eq 1 ]
+			then
+				TicTacToeBoard[$cell]=$player
+				counter=$(($counter+1))
+				echo "board display"
+				displayBoard
+				rowCheck=$(rowCheck)
+				colCheck=$(columnCheck)
+				digCheck=$(diagonalCheck)
+
+			if [ $rowCheck -eq 0 ]
+			then
+					echo "Win"
+					exit;
+			fi
+
+			if [ $colCheck -eq 0 ]
+			then
+					echo "Win"
+					break;
+			fi
+
+			if [ $digCheck -eq 0 ]
+			then
+					echo "Win"
+					break;
+			fi
+
 		else
 			selectSell
 
@@ -82,9 +104,51 @@ function selectSell()
 	done
 }
 
+function rowCheck()
+{
+		row=0
+		for (( i=1; i<=$BOARD_SIZE; i=$((i+3)) ))
+		do
+			if [[ ${TicTacToeBoard[$i]} -eq ${TicTacToeBoard[(($i+1))]} && ${TicTacToeBoard[$i]} -eq ${TicTacToeBoard[(($i+2))]} ]]
+			then
+				row=0
+				break;
+			else
+				row=1
+			fi
+		done
+		echo $row
+}
+
+function columnCheck()
+{
+	column=0
+	for (( i=1; i<4; i++ ))
+	do
+		if [[ ${TicTacToeBoard[$i]} -eq ${TicTacToeBoard[(($i+3))]} && ${TicTacToeBoard[$i]} -eq ${TicTacToeBoard[(($i+6))]} ]]
+		then
+				column=0
+				break;
+		else
+				column=1
+		fi
+	done
+	echo $column
+}
+
+function diagonalCheck()
+{
+	if [[ ${TicTacToeBoard[1]} -eq ${TicTacToeBoard[5]} && ${TicTacToeBoard[1]} -eq ${TicTacToeBoard[9]} || ${TicTacToeBoard[3]} -eq ${TicTacToeBoard[5]} && ${TicTacToeBoard[3]} -eq ${TicTacToeBoard[7]} ]]
+	then
+			diagonal=0
+	else
+			diagonal=1
+	fi
+	echo $diagonal
+}
+
 function main()
 {
-
 resetBoard
 letterAssigned
 whoPlayFirst
